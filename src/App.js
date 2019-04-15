7e9655a6311f52c4175477c23008ec8b578ce12e
Chris Ball <chrisball7@gmail.com>
2019-04-15 15:45:48 -0700

    added application state to app component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,29 @@ import SearchForm from './SearchForm';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      results: [],
+      promptSearch: true
+    }
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  handleSearch(searchResults) {
+    this.setState({
+      results: searchResults,
+      promptSearch: false
+    })
+  }
+
+
   render() {
     return (
       <Fragment>
         <h1>Hello</h1>
-          <SearchResultList></SearchResultList>
-          <SearchForm></SearchForm>
+        <SearchResultList></SearchResultList>
+        <SearchForm></SearchForm>
 
       </Fragment>
     );
